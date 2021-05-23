5833c4d3a0492a91b976e9a0fcc88932706029fd
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-05-23 15:29:53 +0100

    Update dataset.py
    1

--- dataset.py
+++ dataset.py
@@ -7,11 +7,12 @@
 # 2- Alternative is the brown Corpus - comes with nltk 
 # nltk.download('brown')
 from nltk.corpus import brown
-
-
 def get_sentences():
   # returns 57340 of the Brown corpus (around 57340 sentences) - much smaller than Wikipedia but OK for the course 
   # each sentence is represented as a list of individual string tokens
   return brown.sents()      # list of lists, each inner list is a sentence = list of words already tokenized 
 
 
+# 3- Text8Corpus from Gensim
+gensim.models.word2vec.Text8Corpus(fname, max_sentence_length=10000) # Iterate over sentences from the “text8” corpus, unzipped from http://mattmahoney.net/dc/text8.zip.
+# see also LineSentence in word2vec fromm Gensim
