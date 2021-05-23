dacb778488a73393b79e75f93bd66e2b17115e5c
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-05-23 13:42:43 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,17 @@
 
+# 1- 
+# Wikipedia size is 12 GB. You can download chunks of size 100x MB, still need to do parsing as it is XML format.
+
+
+
+# 2- Alternative is the brown Corpus - comes with nltk 
+# nltk.download('brown')
 from nltk.corpus import brown
 
+
 def get_sentences():
-  # returns 57340 of the Brown corpus
+  # returns 57340 of the Brown corpus (around 57340 sentences) - much smaller than Wikipedia but OK for the course 
   # each sentence is represented as a list of individual string tokens
-  return brown.sents()
+  return brown.sents()      # list of lists, each inner list is a sentence = list of words already tokenized 
+
+
