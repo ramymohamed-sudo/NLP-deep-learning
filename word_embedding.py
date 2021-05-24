cbda382df0ea7c8f95719a83ee5877ddef1334b7
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-05-24 09:02:43 +0100

    Update word_embedding.py
    1

--- word_embedding.py
+++ word_embedding.py
@@ -1,3 +1,6 @@
+# how to judge a word embedding scheme: word analogy and word similarty 
+
+
 # Glove and word2vec basic codes from here 
 # https://ibm-learning.udemy.com/course/natural-language-processing-with-deep-learning-in-python/learn/lecture/10770690#overview
 
