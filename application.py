437da9aecf9ff12605ac7cbf05525e8100a39f09
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-05-28 19:23:18 +0100

    Create application.py
    1

---
+++ application.py
@@ -0,0 +1,14 @@
+
+spacy
+Spacy features:
+Tokenization - Lemmatization- PoS
+Dependency Parsing 
+SBD = sentence boundary detection 
+NER and EL = entity linking 
+Similarity and text classification
+Rule-based matching (match a word to other words )
+Training (pre trained by explosion - used to improve statistical models) and Serialization 
+
+
+from textblob import TextBlob	# used for spelling corrections 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
