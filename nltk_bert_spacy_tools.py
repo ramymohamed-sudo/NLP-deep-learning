62a713f7bf795b6087da38155125890bf8afea6d
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-05-28 19:18:58 +0100

    Update nltk_bert_spacy_tools.py
    1

--- nltk_bert_spacy_tools.py
+++ nltk_bert_spacy_tools.py
@@ -204,3 +204,13 @@ matcher = Matcher(nlp.vocab)
 matcher.add('hw', None, pattern)
 matches = matcher(doc)
     
+
+      
+Spacy features:
+Tokenization - Lemmatization- PoS
+Dependency Parsing 
+SBD = sentence boundary detection 
+NER and EL = entity linking 
+Similarity and text classification
+Rule-based matching (match a word to other words )
+Training (pre trained by explosion - used to improve statistical models) and Serialization 
