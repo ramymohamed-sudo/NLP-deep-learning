45d78ad06fb1d88a9d95eb6ad88a3d7133c1aaf3
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-06-12 12:10:18 +0100

    Create text_preprocess.py
    1

---
+++ codes/text_preprocess.py
@@ -0,0 +1,7 @@
+
+
+# cleaning punctuation - stop words - word/sent tokenize and lemmatize
+from string import punctuation
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import WordNetLemmatizer
