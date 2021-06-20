da9267761100737fb7134de691b77856323781a4
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-06-20 16:29:59 +0100

    Update preprocess.py
    1

--- preprocess.py
+++ preprocess.py
@@ -26,7 +26,29 @@ def get_clean(x):
   x = kgp.remove_dups_char(x)
   return x
 
+
+
+
 def correct_word_miss_spelling():
   pass
-
 tweet['text'] = tweet['text'].apply(lambda x: get_clean(x))
+
+
+
+
+def preprocessing(text):
+    words = word_tokenize(text)
+    tokens = [w for w in words if w.lower() not in remove_terms]
+    # you can also try to apply the commented conditions below
+    # stopw = stopwords.word('english')
+    # tokens = [token for token in tokens if token not in stopw]
+    # remove words less than 3 words
+    # tokens = [word for word in tokens if len(word) >=3]
+    # remove remaining tokens that are not alphabetic
+    tokens = [word for word in tokens if word.isalpha()]
+    # lemmatize
+    lemma = WordNetLemmatizer()
+    tokens = [lemma.lemmatize(word) for word in tokens]
+    # here we already have tokens, we join??
+    preprocessed_text = ' '.join(tokens)
+    return preprocessed_text
