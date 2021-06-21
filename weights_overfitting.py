c9700fbd0e82b28f5c67b8a057b66c73d9d895c4
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-06-21 04:02:34 +0100

    Update weights_overfitting.py
    1

--- weights_overfitting.py
+++ weights_overfitting.py
@@ -8,3 +8,12 @@
 # in keras
 model.train_on_batch(x, y)
 weights = model.get_weights()[0]
+
+
+
+
+model.fit(x=x_train, y=y_train, batch_size=128, epochs=20,
+          validation_data=(x_test, y_test),
+          verbose=1, callbacks=[early_stopping, save_best])
+
+glove_embeds = model.layers[0].get_weights()[0]
