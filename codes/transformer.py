ca78f66c8119981c168256d17e52f192a6a7f4f5
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-09-13 10:53:48 +0100

    1

--- codes/transformer.py
+++ codes/transformer.py
@@ -23,6 +23,8 @@ tokenizer = BertTokenizer.from_pretrained(model_name)
 
 txt = ("Given the recent ....")
 tokens = tokenizer.encode_plus(txt, max_length=512, truncation=True, padding=max_length, add_special_tokens=True, return_tensors='tf')  # 'pt' for Pytorch
+[CLS] = 101, [SEP] = 102, [MASK] = 103, [UNK] = 100, [PAD] = 0, 
+print(tokens)   # represented by a dict
 
 
 
